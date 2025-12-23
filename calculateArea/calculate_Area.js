ab2d7e0ce9ec17fdfa75f6cb4d544bb8db4af7ea
ortizSimon <ortiz.simon211@gmail.com>
2025-12-23 14:30:47 -0500

    grocerie tracker

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,6 +1,7 @@
 let length;
 let width;
 
+
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
